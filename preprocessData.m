5bd6431d3a8786aa9fa5b0ff933aa44fc4d233b9
krupeshp19 <krupeshpethani2001@gmail.com>
2024-06-19 14:41:15 +0200

    updated func for preprocessing data

--- preprocessData.m
+++ preprocessData.m
@@ -1,46 +1,30 @@
 function preprocessedData = preprocessData(activityData)
-    % This function preprocesses the synthetic activity data by normalizing
-    % and filtering the accelerometer and gyroscope data.
-
-    % Extract raw data
+    % Normalize the accelerometer and gyroscope data
     accelData = activityData.accel;
     gyroData = activityData.gyro;
-
-    % Normalize the accelerometer data
-    normAccel = (accelData - mean(accelData)) / std(accelData);
-
-    % Normalize the gyroscope data
-    normGyro = (gyroData - mean(gyroData)) / std(gyroData);
-
-    % Apply a low-pass filter to the normalized data
-    % Design a low-pass filter
-    fs = 50; % Sampling frequency (Hz)
-    fc = 5; % Cutoff frequency (Hz)
-    [b, a] = butter(4, fc / (fs / 2), 'low'); % 4th order Butterworth filter
-
-    % Filter the accelerometer data
-    filtAccel = filtfilt(b, a, normAccel);
-
-    % Filter the gyroscope data
-    filtGyro = filtfilt(b, a, normGyro);
-
+    
+    accelData = normalize(accelData, 2);
+    gyroData = normalize(gyroData, 2);
+    
+    % Apply a low-pass filter to remove noise
+    % Design a low-pass filter with a cutoff frequency of 0.1 Hz
+    [b, a] = butter(2, 0.1, 'low');
+    for i = 1:3
+        accelData(i, :) = filtfilt(b, a, accelData(i, :));
+        gyroData(i, :) = filtfilt(b, a, gyroData(i, :));
+    end
+    
     % Package the preprocessed data into a struct
     preprocessedData.time = activityData.time;
-    preprocessedData.accel = filtAccel;
-    preprocessedData.gyro = filtGyro;
-
+    preprocessedData.accel = accelData;
+    preprocessedData.gyro = gyroData;
+    
     % Plot the preprocessed data for visual verification
     figure;
-    subplot(2, 1, 1);
-    plot(activityData.time, filtAccel);
-    title('Preprocessed Accelerometer Data');
-    xlabel('Time (s)');
-    ylabel('Normalized Acceleration (m/s^2)');
-
-    subplot(2, 1, 2);
-    plot(activityData.time, filtGyro);
-    title('Preprocessed Gyroscope Data');
-    xlabel('Time (s)');
-    ylabel('Normalized Angular Velocity (rad/s)');
-
+    subplot(3, 2, 1); plot(preprocessedData.time, accelData(1, :)); title('Accelerometer X-axis');
+    subplot(3, 2, 2); plot(preprocessedData.time, gyroData(1, :)); title('Gyroscope X-axis');
+    subplot(3, 2, 3); plot(preprocessedData.time, accelData(2, :)); title('Accelerometer Y-axis');
+    subplot(3, 2, 4); plot(preprocessedData.time, gyroData(2, :)); title('Gyroscope Y-axis');
+    subplot(3, 2, 5); plot(preprocessedData.time, accelData(3, :)); title('Accelerometer Z-axis');
+    subplot(3, 2, 6); plot(preprocessedData.time, gyroData(3, :)); title('Gyroscope Z-axis');
 end
