678d073e49148dbf2e471d11846f91dcbd5249a9
krupeshp19 <krupeshpethani2001@gmail.com>
2024-06-19 14:42:10 +0200

    extract more features and fixes

--- extractFeatures.m
+++ extractFeatures.m
@@ -1,53 +1,28 @@
-function features = extractFeatures(preprocessedData, windowSize, overlap)
-    % This function extracts relevant features from the preprocessed activity
-    % data. Features are calculated over sliding windows.
-
-    % Parameters
-    % windowSize - The size of the sliding window (in samples)
-    % overlap - The number of overlapping samples between consecutive windows
-
-    % Extract preprocessed data
+function features = extractFeatures(preprocessedData)
     accelData = preprocessedData.accel;
     gyroData = preprocessedData.gyro;
-    time = preprocessedData.time;
-
-    % Initialize feature arrays
-    numWindows = floor((length(accelData) - windowSize) / (windowSize - overlap)) + 1;
-    meanAccel = zeros(numWindows, 1);
-    stdAccel = zeros(numWindows, 1);
-    rmsAccel = zeros(numWindows, 1);
-    meanGyro = zeros(numWindows, 1);
-    stdGyro = zeros(numWindows, 1);
-    rmsGyro = zeros(numWindows, 1);
-
-    % Calculate features over sliding windows
-    for i = 1:numWindows
-        startIdx = (i - 1) * (windowSize - overlap) + 1;
-        endIdx = startIdx + windowSize - 1;
-        if endIdx > length(accelData)
-            break;
-        end
-
-        % Extract window data
-        windowAccel = accelData(startIdx:endIdx);
-        windowGyro = gyroData(startIdx:endIdx);
-
-        % Calculate features
-        meanAccel(i) = mean(windowAccel);
-        stdAccel(i) = std(windowAccel);
-        rmsAccel(i) = rms(windowAccel);
-        meanGyro(i) = mean(windowGyro);
-        stdGyro(i) = std(windowGyro);
-        rmsGyro(i) = rms(windowGyro);
-    end
-
-    % Package the extracted features into a struct
-    features.meanAccel = meanAccel;
-    features.stdAccel = stdAccel;
-    features.rmsAccel = rmsAccel;
-    features.meanGyro = meanGyro;
-    features.stdGyro = stdGyro;
-    features.rmsGyro = rmsGyro;
-    features.time = time(1:windowSize-overlap:endIdx);
-
+    
+    % Initialize feature struct
+    features = struct;
+    
+    % Feature extraction function
+    extractStatFeatures = @(data) struct( ...
+        'mean', mean(data, 2), ...
+        'std', std(data, 0, 2), ...
+        'rms', rms(data, 2), ...
+        'max', max(data, [], 2), ...
+        'min', min(data, [], 2), ...
+        'energy', sum(data .^ 2, 2));
+    
+    % Extract features for accelerometer data
+    features.accel = extractStatFeatures(accelData);
+    
+    % Extract features for gyroscope data
+    features.gyro = extractStatFeatures(gyroData);
+    
+    % Calculate correlations between accelerometer and gyroscope data
+    features.corr = struct( ...
+        'accel_gyro_x', corr(accelData(1, :)', gyroData(1, :)'), ...
+        'accel_gyro_y', corr(accelData(2, :)', gyroData(2, :)'), ...
+        'accel_gyro_z', corr(accelData(3, :)', gyroData(3, :)'));
 end
